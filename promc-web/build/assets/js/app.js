016096216d2f24236a625a416f11dcd1b4c6ce99
Vedran Jaic <vedranjaic@icloud.com>
2016-11-13 02:15:22 +0100

    Initial commit

---
+++ promc-web/build/assets/js/app.js
@@ -0,0 +1,42 @@
+// $(".navbar-nav a").click(function() {
+// 	var page = $(this).attr("href");
+// 	console.log(page);
+//     $('html, body').animate({
+//         scrollTop: $(page).offset().top
+//     }, 500);
+// });
+
+
+// --- [ Scroll to top ]
+// init controller
+	var controller = new ScrollMagic.Controller();
+
+	// build tween
+	var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+
+	// build scene
+	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
+					.setTween(tween)
+					// .addIndicators() // add indicators (requires plugin)
+					.addTo(controller);
+
+	// change behaviour of controller to animate scroll instead of jump
+	controller.scrollTo(function (newpos) {
+		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+	});
+
+	//  bind scroll to anchor links
+	$(document).on("click", ".navbar-nav a[href^='#']", function (e) {
+		var id = $(this).attr("href");
+		if ($(id).length > 0) {
+			e.preventDefault();
+
+			// trigger scroll
+			controller.scrollTo(id);
+
+				// if supported by the browser we can even update the URL.
+			if (window.history && window.history.pushState) {
+				history.pushState("", document.title, id);
+			}
+		}
+	});
