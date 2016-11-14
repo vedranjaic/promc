d51cf2a61388a8c6c1f51fe79dd9bfd4feffb267
Vedran Jaic <vedranjaic@icloud.com>
2016-11-14 03:33:39 +0100

    Styled prototype

--- promc-web/src/js/app.js
+++ promc-web/src/js/app.js
@@ -1,42 +1,49 @@
-// $(".navbar-nav a").click(function() {
-// 	var page = $(this).attr("href");
-// 	console.log(page);
-//     $('html, body').animate({
-//         scrollTop: $(page).offset().top
-//     }, 500);
-// });
-
-
 // --- [ Scroll to top ]
 // init controller
-	var controller = new ScrollMagic.Controller();
+var controller = new ScrollMagic.Controller();
 
-	// build tween
-	var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
+// build tween
+var tween = TweenMax.from("#animate", 0.5, {autoAlpha: 0, scale: 0.7});
 
-	// build scene
-	var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
-					.setTween(tween)
-					// .addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
+// build scene
+var scene = new ScrollMagic.Scene({triggerElement: "a#top", duration: 200, triggerHook: "onLeave"})
+				.setTween(tween)
+				// .addIndicators() // add indicators (requires plugin)
+				.addTo(controller);
 
-	// change behaviour of controller to animate scroll instead of jump
-	controller.scrollTo(function (newpos) {
-		TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
-	});
+// change behaviour of controller to animate scroll instead of jump
+controller.scrollTo(function (newpos) {
+	TweenMax.to(window, 0.5, {scrollTo: {y: newpos}});
+});
 
-	//  bind scroll to anchor links
-	$(document).on("click", ".navbar-nav a[href^='#']", function (e) {
-		var id = $(this).attr("href");
-		if ($(id).length > 0) {
-			e.preventDefault();
+//  bind scroll to anchor links
+$(document).on("click", ".navbar-nav a[href^='#']", function (e) {
+	var id = $(this).attr("href");
+	if ($(id).length > 0) {
+		e.preventDefault();
 
-			// trigger scroll
-			controller.scrollTo(id);
+		// trigger scroll
+		controller.scrollTo(id);
 
-				// if supported by the browser we can even update the URL.
-			if (window.history && window.history.pushState) {
-				history.pushState("", document.title, id);
-			}
+			// if supported by the browser we can even update the URL.
+		if (window.history && window.history.pushState) {
+			history.pushState("", document.title, id);
 		}
-	});
+	}
+});
+
+
+// --- [ Menu icon ]
+(function() {
+  var i, resize;
+
+  $(".navbar-toggle").click(function() {
+    clearInterval(i);
+    return $(".navbar-toggle").toggleClass("cross");
+  });
+
+  $(window).resize(resize);
+
+  resize();
+
+}).call(this);
