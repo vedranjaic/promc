8530a1e0185c445969dee29b9e37ddc818ba41dd
Vedran Jaic <vedranjaic@icloud.com>
2017-01-27 10:47:09 +0100

    Localization hr

---
+++ promc-web/build/assets/js/localizations.js
@@ -0,0 +1,162 @@
+String.toLocaleString({
+	"en": {
+		"%title": "PRO Moving Company | Profesional movers",
+		"%info": "TODO",
+		"nav": {
+			"%intro": "Intro",
+			"%services": "Services",
+			"%clients": "Clients",
+			"%aboutus": "About us",
+			"%contact": "Contact"
+		}
+	},
+	"de": {
+		"%title": "PRO Moving Company | Profesional movers",
+		"%info": "TODO",
+		"nav": {
+			"%intro": "TODO",
+			"%services": "TODO",
+			"%clients": "TODO",
+			"%aboutus": "TODO",
+			"%contact": "TODO"
+		}
+	},
+	"hr": {
+		"%title": "PRO Moving Company | Profesional movers",
+		"%info": "Međunarodna selidba ureda i arhive - Specijalisti za korporativne selidbe",
+		"nav": {
+			"%intro": "Uvod",
+			"%services": "Usluge",
+			"%clients": "Klijenti",
+			"%aboutus": "O nama",
+			"%contact": "Kontakt"
+		},
+		"page": {
+			"intro": {
+				"%title": "Professional movers",
+				"%lead": "Profesionalan i korektan odnos prema klijentima za nas je stabilan i održiv način poslovanja."
+			},
+			"services": {
+				"%details": "Detalji usluge",
+				"office": {
+					"%title": "Selidbe ureda i arhive",
+					"%lead": "Specijalisti za korporativne selidbe",
+					"%p1": "U ProMC-u posebnu pažnju poklanjamo logističkoj razradi planova korporativnih i uredskih preseljenja, u koordinaciji s predstavnicima Vaše tvrtke.",
+					"%p2": "Preseljenje velikog broja zaposlenika u kratkom vremenskom roku iz jednog poslovnog prostora u drugi, uključujući arhivu naša je specijalnost. Bez obzira na opseg projekta garantiramo profesionalnu zaštitu sve elektroničke opreme, uredskog namještaja te svih koridora i liftova kojima bismo se prilikom preseljenja služili.",
+					"%p3": "Fleksibilnost radnog vremena te brzinu i kvalitetu usluge stavljamo na prvo mjesto i ispunjavamo sva Vaša očekivanja bez kompromisa jer smatramo da je naš posao završen tek sa zadovoljstvom našeg klijenta, sve dok i posljednja pomno uzgajana uredska biljka ne nađe svoje novo mjesto pod suncem."
+				},
+				"home": {
+					"%title": "Selidbe stana/kuće",
+					"%lead": "Pedantno i sigurno",
+					"%p1": "Neovisno da li se selite privatno ili zbog promjene radnog mjesta te sami ili s obitelji obratite nam se s punim povjerenjem.",
+					"%p2": "Služeći se najnovnijim pakirnim materijalima i opremom, pružit ćemo Vam uslugu profesionalnog pakiranja i zaštite osobnih stvari, namještaja i umjetnina, prijevoz te raspakiravanje. A nakon što i posljednja Vaša stvar uđe u novi prostor možemo Vam ponudit uslugu montaže koju obavlja naš domar, bez obzira radi li se o električarskim, vodointalaterskim ili stolarskim poslovima.",
+					"%p3": "Naposlijetku, uslugu detaljnog čišćenja Vašeg novog doma nudimo kao posljednju etapu procesa kroz koji Vam uz uslugu  želimo takvu veliku životnu promjenu učiniti što manje stresnom.",
+					"%p4": "Jer za nas nije važno selite li na drugi kraj ulice ili zemlje. Naš je cilj da Vas na kraju uselimo u prostor koji nećete nazivati stanom ili kućom već domom."
+				},
+				"international": {
+					"%title": "Međunarodna selidba",
+					"%lead": "Selidbe bez granica",
+					"%p1": "Stanovnici Europske Unije sele se danas više nego ikada prije. Ako ste i Vi jedan od mnogih koji sele u Europu, neovisno radi li se o nekoliko kutija ili cijelom kućanstvu,  možemo Vam ponuditi sveobuhvatnu uslugu od vrata do vrata, u jednom potezu bez pretovara umanjujući broj rukovanja s Vašom robom, a time i rizik od oštećenja.  Zahvaljujući našem iskusnom i profesionalnom osoblju sva Vaša imovina i dragocjenosti biti će pažljivo zapakirane te zaštićene tijekom cijelog procesa preseljenja. To garantiramo svojim pristupom prema Vašim osobnim stvarima poštujući njihovu jedinstvenost i emociju koja vlasnika često veže u pojedine predmete.",
+					"%p2": "Prije samog preseljena u dogovoru s Vama izrađujemo deteljan plan prilagođen Vašim potrebama navodeći sve pojedinosti prije i nakon preseljenja. Za Vas ćemo se pobrinuti za rješavanje i pravilno ispunjavanje sve popratne dokumentacije te u suradnji s našim vanjskim partnerima pomoći s carinskim pitanjima. Pružit ćemo Vam korisne informacije o samom procesu preseljenja te predradnjama koje bi bilo dobro odraditi prije dolaska našeg osoblja.",
+					"%p3": "Omogućit ćemo Vam i privremeno ili dugotrajno pohranjivanje Vaših stvari u našem sigurnom skladištu ukoliko ih nemate potrebu sve preseliti i koristiti na novoj lokaciji.",
+					"%p4": "Obratite nam se s povjerenjem."
+				},
+				"archive": {
+					"%title": "Skladištenje arhive, namještaja i osobnih stvari",
+					"%lead": "Zaštićeno, sigurno i brižno pohranjeno",
+					"%p1": "Problem pohranjivanja arhive čest je među tvrtkama zbog visokih cijena najma poslovnih prostora ili neadekvatnog  i nedovoljno velikog prostora. U ProMC-u imamo upravo ono što Vam treba.  Unutar našeg zaštićenog skladišta imamo posebno odvojen prostor samo za pohranu arhive kapaciteta nekoliko tisuća registratora čime je mogućnost gubitka ili oštećenja Vaše dokumentacije svedena na minimum.",
+					"%p2": "Vaša arhiva cijelo je vrijeme potpuno dostupna samo Vama u bilo koje doba dana kao da se nalazi u Vašem poslovnom prostoru samo bez plaćanja visoke cijene najma.",
+					"%p3": "U našem skladištu možete potpuno sigurno i bez ikakve brige pohraniti namještaj te osobne stvari na dulje ili kraće periode, ovisno o Vašim potrebama. Pritom su sve pohranjene stvari adekvatno zapakirane i zaštićene od vanjskih utjecaja kako bi Vas dočekale u jednakom stanju u kakvom su pohranjene."
+				},
+				"questionnaire": {
+					"%tab1": "Ured",
+					"%tab2": "Stan",
+					"%tab3": "Međunarodna",
+					"%tab3": "Skladištenje",
+					"tabs": {
+						"%details": "Detalji selidbe",
+						"%additionalDetails": "Dodatne informacije",
+						"%loading": "Molimo pričekajte...",
+						"%submit": "Kontaktirajte me",
+						"%successAlert": "Vaša poruka uspješno je poslana!",
+						"%successTitle": "Zahvaljujemo na upitu",
+						"%successMessage": "Naš agent će Vas kontaktirati u najskorijem mogućem roku. Kopija ove poruke poslana je i na Vašu e-mail adresu.",
+						"%successButton": "Pošalji još jedan upit",
+						"office": {
+							"%formAlertTitle": "Obvezna polja!",
+							"%formAlertMessage": "Za kontakt potrebno je popuniti polja s kontaktnim podacima.",
+							"%formLabelName": "Kontakt osoba",
+							"%formLabelEmail": "E-mail adresa",
+							"%formLabelNumber": "Kontakt broj",
+							"%formAddressStart": "Preseljenje iz",
+							"%formDateStart": "Datum preseljenja",
+							"%formAddressFinish": "Preseljenje u",
+							"%formEmployees": "Broj radnih mjesta"
+						},
+						"home": {
+							"%formAlertTitle": "Obvezna polja!",
+							"%formAlertMessage": "Za kontakt potrebno je popuniti polja s kontaktnim podacima.",
+							"%formLabelName": "Ime i prezime",
+							"%formLabelEmail": "E-mail adresa",
+							"%formLabelNumber": "Kontakt broj",
+							"%formAddressStart": "Preseljenje iz",
+							"%formDateStart": "Datum preseljenja",
+							"%formAddressFinish": "Preseljenje u",
+							"%formEmployees": "Broj radnih mjesta"
+						},
+						"international": {
+							"%formAlertTitle": "Obvezna polja!",
+							"%formAlertMessage": "Za kontakt potrebno je popuniti polja s kontaktnim podacima.",
+							"%formLabelName": "Ime i prezime",
+							"%formLabelEmail": "E-mail adresa",
+							"%formLabelNumber": "Kontakt broj",
+							"%formAddressStart": "Preseljenje iz",
+							"%formDateStart": "Datum preseljenja",
+							"%formAddressFinish": "Preseljenje u",
+							"%formEmployees": "Broj radnih mjesta"
+						},
+						"archive": {
+							"%formAlertTitle": "Obvezna polja!",
+							"%formAlertMessage": "Za kontakt potrebno je popuniti polja s kontaktnim podacima.",
+							"%formLabelName": "Ime i prezime",
+							"%formLabelEmail": "E-mail adresa",
+							"%formLabelNumber": "Kontakt broj",
+							"%formItemType": "Vrsta robe"
+						}
+					}
+				}
+			},
+			"clients": {
+				"%title": "Naši klijenti",
+				"%lead": "Preselite se među naše klijente"
+			},
+			"aboutus": {
+				"%title": "O nama",
+				"%lead": "Professional movers",
+				"%p1": "Pro Moving Company d.o.o. je osnovan kao rezultat višegodišnjeg znanja i iskustva njenih osnivača u sektoru korporativnih i međunarodnih preseljenja te prijevoza teških tereta čime smo specijalizirani upravo za takvu vrstu djelatnosti.",
+				"%p2": "Čine je pristupačni i stručni ljudi osposobljeni prema tradicijskim europskim standardima koje u praksi primijenjuju na domaćem i inozemnom tržištu.",
+				"%p3": "Želja za samostalnošću te radom na drugačiji i učinkovitiji - nama jedini prihvatljiv način - bile su nam vodilje pri osnivanju tvrtke. To znači da kontinuirano primijenjujemo stečena znanja za dobrobit klijenta čime želimo graditi odnose međusobnog poštovanja i polagati temelje dugogodišnje suradnje.",
+				"%p4": "Svako preseljenje je posebno i zahtjevi klijenata se jako razlikuju pa je stoga naša vizija pružanje logističkih usluga krojenih prema specifičnim potrebama svakog klijenta kroz savjetovanja te etapno planiranje posla, povećavajući efikasnost i pritom umanjujući ukupne troškove za obje strane.",
+				"%p5": "Slijedom toga, naša je misija da kontinuirano radimo na provedbi logističkih rješenja kroz preseljenja, dostave i skladištenja, služeći se specijaliziranom opremom te vodeći brigu o propisanim standardima i unutar dogovorenog roka – što je apsolutni imperativ.",
+				"stats": {
+					"numberClosets": "preseljenih ormara",
+					"numberBoxes": "preseljenih kutija",
+					"numberChairs": "preseljenih stolaca",
+					"numberFlowers": "preseljenih cvijetova"
+				}
+			},
+			"contact": {
+				"%title": "Kontaktirajte nas",
+				"%lead": "Stojimo Vam na raspolaganju",
+				"%addressCountry": "Hrvatska",
+				"%detailsOIB": "OIB",
+				"%detailsBank": "Banka",
+				"%detailsIBAN": "IBAN",
+				"%detailsSWIFT": "SWIFT",
+				"social": {
+					"facebook": "Posjetite nas na Facebooku"
+				}
+			}
+		}
+	}
+});
