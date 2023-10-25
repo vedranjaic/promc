0b22852ff7e47e8f034f6bd54e1ef44e71f6de5b
Vedran Jaic <vedran.jaic@neos.hr>
2023-10-25 19:18:44 +0200

    Privacy policy EN and DE localizations

--- promc-web/build/assets/js/localization.js
+++ promc-web/build/assets/js/localization.js
@@ -1,6 +1,9 @@
 // --- [ l10n.js ]
 // On first load set language in cookie to 'hr'
-// setCookie("promcLocale", "hr");
+if (!getCookie("promcLocale")) {
+	setCookie("promcLocale", "hr");
+}
+
 // Change language on dropdown
 $('#lang-hr').click(function() {
 	setCookie("promcLocale", $(this).text().toLowerCase(), 365);
@@ -100,7 +103,7 @@ document.getElementById('pageLeadSelidbeInternational').innerText = l('page')['s
 document.getElementById('textSelidbeInternationalp1').innerText = l('page')['services']['international']['p1'];
 document.getElementById('textSelidbeInternationalp2').innerText = l('page')['services']['international']['p2'];
 document.getElementById('textSelidbeInternationalp3').innerText = l('page')['services']['international']['p3'];
-document.getElementById('textSelidbeInternationalp4').innerText = l('page')['services']['international']['p4'];
+// document.getElementById('textSelidbeInternationalp4').innerText = l('page')['services']['international']['p4'];
 // Arhiva
 document.getElementById('pageTitleSelidbeArhiva').innerText = l('page')['services']['archive']['title'];
 document.getElementById('pageLeadSelidbeArhiva').innerText = l('page')['services']['archive']['lead'];
@@ -183,6 +186,119 @@ document.getElementById('qArchiveLabelContactName').innerText = l('questionnaire
 document.getElementById('qArchiveLabelContactEmail').innerText = l('questionnaire')['tabs']['archive']['formLabelEmail'];
 document.getElementById('qArchiveLabelContactNumber').innerText = l('questionnaire')['tabs']['archive']['formLabelNumber'];
 document.getElementById('qDetailsType').innerText = l('questionnaire')['tabs']['archive']['formItemType'];
+// Cookie message
+document.getElementById('cmIntroTextp1').innerText = l('cookiemessage')['cmIntroText'];
+document.getElementById('cmIntroTexta1').innerText = l('cookiemessage')['cmIntroLink'];
+document.getElementById('cmCookie1Title').innerText = l('cookiemessage')['cmcookies']['cmCookieBasicTitle'];
+document.getElementById('cmCookie1Desc').innerText = l('cookiemessage')['cmcookies']['cmCookieBasicDesc'];
+document.getElementById('cmCookie2Title').innerText = l('cookiemessage')['cmcookies']['cmCookieGATitle'];
+document.getElementById('cmCookie2Desc').innerText = l('cookiemessage')['cmcookies']['cmCookieGADesc'];
+document.getElementById('cookie-settings--btn').innerText = l('cookiemessage')['cmBtnSettings'];
+document.getElementById('cookie-accept').innerText = l('cookiemessage')['cmBtnAccept'];
+// Privacy policy
+document.getElementById('ppModalTitle').innerText = l('privacypolicy')['ppModalTitle'];
+document.getElementById('ppIntroTextp1').innerText = l('privacypolicy')['ppIntro']['ppIntroTextp1'];
+document.getElementById('ppIntroTextp2').innerText = l('privacypolicy')['ppIntro']['ppIntroTextp2'];
+document.getElementById('ppPolicy01Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy01Titleh1'];
+document.getElementById('ppPolicy01Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy01Textp1'];
+document.getElementById('ppPolicy02Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy02Titleh1'];
+document.getElementById('ppPolicy02Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy02Textp1'];
+document.getElementById('ppPolicy02Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy02Textp2'];
+document.getElementById('ppPolicy02Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy02Textp3'];
+document.getElementById('ppPolicy03Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Titleh1'];
+document.getElementById('ppPolicy03Titleh2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Titleh2'];
+document.getElementById('ppPolicy03Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Textp1'];
+document.getElementById('ppPolicy03Titleh3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Titleh3'];
+document.getElementById('ppPolicy03Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Textp2'];
+document.getElementById('ppPolicy03Titleh4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Titleh4'];
+document.getElementById('ppPolicy03Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Textp3'];
+document.getElementById('ppPolicy03Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Textp3'];
+document.getElementById('ppPolicy03Textp4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy03Textp4'];
+document.getElementById('ppPolicy04Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy04Titleh1'];
+document.getElementById('ppPolicy04Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy04Textp1'];
+document.getElementById('ppPolicy05Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy05Titleh1'];
+document.getElementById('ppPolicy05Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy05Textp1'];
+document.getElementById('ppPolicy06Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Titleh1'];
+document.getElementById('ppPolicy06Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textp1'];
+document.getElementById('ppPolicy06Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli1'];
+document.getElementById('ppPolicy06Textli2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli2'];
+document.getElementById('ppPolicy06Textli3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli3'];
+document.getElementById('ppPolicy06Textli4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli4'];
+document.getElementById('ppPolicy06Textli5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli5'];
+document.getElementById('ppPolicy06Textli6').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli6'];
+document.getElementById('ppPolicy06Textli7').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli7'];
+document.getElementById('ppPolicy06Textli8').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli8'];
+document.getElementById('ppPolicy06Textli9').innerText = l('privacypolicy')['ppPolicies']['ppPolicy06Textli9'];
+document.getElementById('ppPolicy07Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Titleh1'];
+document.getElementById('ppPolicy07Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Textp1'];
+document.getElementById('ppPolicy07Titleh2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Titleh2'];
+document.getElementById('ppPolicy07Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Textp2'];
+document.getElementById('ppPolicy07Titleh3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Titleh3'];
+document.getElementById('ppPolicy07Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Textp3'];
+document.getElementById('ppPolicy07Titleh4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Titleh4'];
+document.getElementById('ppPolicy07Textp4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Textp4'];
+document.getElementById('ppPolicy07Titleh5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Titleh5'];
+document.getElementById('ppPolicy07Textp5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy07Textp5'];
+document.getElementById('ppPolicy08Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy08Titleh1'];
+document.getElementById('ppPolicy08Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy08Textp1'];
+document.getElementById('ppPolicy09Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy09Titleh1'];
+document.getElementById('ppPolicy09Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy09Textp1'];
+document.getElementById('ppPolicy09Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy09Textli1'];
+document.getElementById('ppPolicy10Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Titleh1'];
+document.getElementById('ppPolicy10Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textp1'];
+document.getElementById('ppPolicy10Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textp2'];
+document.getElementById('ppPolicy10Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textli1'];
+document.getElementById('ppPolicy10Textli2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textli2'];
+document.getElementById('ppPolicy10Textli3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textli3'];
+document.getElementById('ppPolicy10Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textp3'];
+document.getElementById('ppPolicy10Textp4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy10Textp4'];
+document.getElementById('ppPolicy11Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy11Titleh1'];
+document.getElementById('ppPolicy11Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy11Textp1'];
+document.getElementById('ppPolicy12Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy12Titleh1'];
+document.getElementById('ppPolicy12Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy12Textp1'];
+document.getElementById('ppPolicy13Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy13Titleh1'];
+document.getElementById('ppPolicy13Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy13Textp1'];
+document.getElementById('ppPolicy14Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh1'];
+document.getElementById('ppPolicy14Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp1'];
+document.getElementById('ppPolicy14Titleh2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh2'];
+document.getElementById('ppPolicy14Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp2'];
+document.getElementById('ppPolicy14Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textli1'];
+document.getElementById('ppPolicy14Textli2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textli2'];
+document.getElementById('ppPolicy14Textli3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textli3'];
+document.getElementById('ppPolicy14Textli4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textli4'];
+document.getElementById('ppPolicy14Textp3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp3'];
+document.getElementById('ppPolicy14Titleh3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh3'];
+document.getElementById('ppPolicy14Textp4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp4'];
+document.getElementById('ppPolicy14Titleh4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh4'];
+document.getElementById('ppPolicy14Textp5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp5'];
+document.getElementById('ppPolicy14Titleh5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh5'];
+document.getElementById('ppPolicy14Textp6').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp6'];
+document.getElementById('ppPolicy14Titleh6').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh6'];
+document.getElementById('ppPolicy14Textp7').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp7'];
+document.getElementById('ppPolicy14Titleh7').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Titleh7'];
+document.getElementById('ppPolicy14Textp8').innerText = l('privacypolicy')['ppPolicies']['ppPolicy14Textp8'];
+document.getElementById('ppPolicy15Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy15Titleh1'];
+document.getElementById('ppPolicy15Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy15Textp1'];
+document.getElementById('ppPolicy16Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy16Titleh1'];
+document.getElementById('ppPolicy16Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy16Textp1'];
+document.getElementById('ppPolicy16Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy16Textli1'];
+document.getElementById('ppPolicy16Textli2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy16Textli2'];
+document.getElementById('ppPolicy16Textli3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy16Textli3'];
+document.getElementById('ppPolicy17Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy17Titleh1'];
+document.getElementById('ppPolicy17Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy17Textp1'];
+document.getElementById('ppPolicy18Titleh1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Titleh1'];
+document.getElementById('ppPolicy18Textp1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textp1'];
+document.getElementById('ppPolicy18Textli1').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli1'];
+document.getElementById('ppPolicy18Textli2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli2'];
+document.getElementById('ppPolicy18Textli3').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli3'];
+document.getElementById('ppPolicy18Textli4').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli4'];
+document.getElementById('ppPolicy18Textli5').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli5'];
+document.getElementById('ppPolicy18Textp2').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textp2'];
+document.getElementById('ppPolicy18Textli6').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli6'];
+document.getElementById('ppPolicy18Textli7').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli7'];
+document.getElementById('ppPolicy18Textli8').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli8'];
+document.getElementById('ppPolicy18Textli9').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli9'];
+document.getElementById('ppPolicy18Textli10').innerText = l('privacypolicy')['ppPolicies']['ppPolicy18Textli10'];
 
 document.documentElement.lang = String.locale || document.documentElement.lang;
 
